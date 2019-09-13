b117b134b0943354de7939c1230fed2f6b90884d
hodongtu <hodongtu@gmail.com>
2019-09-13 09:40:20 +0700

    update app route

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/idecision', defaults={'path': ''})
+@app.route('/idecision/<path:path>')
 def index(path):
     return render_template('index.html')
-
-if __name__ == '__main__':
-	app.run(port=5444)
